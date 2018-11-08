b4114d42d695fe1f4e34fcfc91047f49292e352c
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2018-11-08 16:22:18 -0600

    Signup and Signin Mutations finished

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -63,7 +63,32 @@ const Mutations = {
     });
     // Return the user to the browser
     return user;
+  },
+  async signin(parent, { name, password }, ctx, info) {
+    // check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for username ${name}`);
+    }
+    // check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid Password');
+    }
+    // generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // return the user
+    return user;
   }
+  // signout(parent, args, ctx, info) {
+  //   ctx.response.clearCookie('token');
+  //   return { message: 'Goodbye' };
+  // }
 };
 
 module.exports = Mutations;
