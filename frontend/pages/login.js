2ca8f822b0eb110d9b6272902f8fa283c9e052c5
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-11-13 07:56:33 -0800

    adds login functionality

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+import Signin from '../components/Signin';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
 
 const Login = () => (
   <div>
-    <p>This is the Login page</p>
+    <Columns>
+      <Signin />
+    </Columns>
     <Link href="/triplist">
       <button>Complete</button>
     </Link>
