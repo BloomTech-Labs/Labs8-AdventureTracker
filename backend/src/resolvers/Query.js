75198d428b02a9348b9800fa3108026a4e2a2046
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-11-13 08:18:59 -0800

    updates queries for login functionality

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,23 @@
 const { forwardTo } = require('prisma-binding');
 const Query = {
   // trips: forwardTo('db'),
-  users: forwardTo('db')
+  users: forwardTo('db'),
   // async trips(parent, args, ctx, info) {
   //   const trips = await ctx.db.query.trips();
   //   return trips;
   // }
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
