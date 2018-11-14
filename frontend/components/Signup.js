4478a77bed9ebcb21cf88b6e7911a5856e8ebc14
Simon Davis <Simon_Freelance@hotmail.com>
2018-11-13 19:24:40 -0500

    Changed Form import

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import Form from './styles/Form';
+import { Form } from './styles/FormStyles';
 import Error from './ErrorMessage';
 
 const SIGNUP_MUTATION = gql`
