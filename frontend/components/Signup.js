1417c50c68bb7409d19f3d596621d7dc9941364d
Simon Davis <Simon_Freelance@hotmail.com>
2018-11-13 21:15:41 -0500

    Replaced elements with styled components and added step transitisions

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Form } from './styles/FormStyles';
+import {
+  Form,
+  FormLabel,
+  FormHeader,
+  FormBox,
+  FormGroup,
+  FormFieldset,
+  FormTitle
+} from './styles/FormStyles';
+import styled from 'styled-components';
+import { PrimaryBtn } from './styles/ButtonStyles';
 import Error from './ErrorMessage';
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const BackBtn = styled(PrimaryBtn)`
+  background: ${props => props.theme.opacitygrey};
+`;
+const NextBtn = styled(PrimaryBtn)`
+  margin: 0 0 0 auto;
+`;
 
+const SignUpBtn = styled(PrimaryBtn)`
+  margin: 0 0 0 auto;
+`;
 const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
     signup(email: $email, name: $name, password: $password) {
@@ -19,14 +42,24 @@ class Signup extends Component {
   state = {
     name: '',
     email: '',
-    password: ''
+    password: '',
+    password2: '',
+    step: 1
   };
 
   updateState = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  prevStep = () => {
+    this.setState({ step: --this.state.step });
+  };
+  nextStep = () => {
+    this.setState({ step: ++this.state.step });
+  };
+
   render() {
+    const { step } = this.state;
     return (
       // using the SIGNUP_MUTATION sending the state
       <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
@@ -38,46 +71,97 @@ class Signup extends Component {
               onSubmit={async e => {
                 e.preventDefault();
                 await signup();
-                this.setState({ name: '', email: '', password: '' });
+                this.setState({ name: '', email: '', password: '', password2: '' });
               }}
             >
+              <FormHeader>Adventure Tracker</FormHeader>
               {/* updates the bar with loading status */}
-              <fieldset disabled={loading} aria-busy={loading}>
-                <h2>Sign Up for an Account</h2>
+              <FormFieldset disabled={loading} aria-busy={loading}>
+                <FormTitle>Sign-Up</FormTitle>
                 {/* TODO Clean up error messages */}
                 <Error error={error} />
-                <label htmlFor="email">
-                  Email
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="enter your email"
-                    value={this.state.email}
-                    onChange={this.updateState}
-                  />
-                </label>
-                <label htmlFor="name">
-                  Name
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="enter your name"
-                    value={this.state.name}
-                    onChange={this.updateState}
-                  />
-                </label>
-                <label htmlFor="password">
-                  Password
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="enter your password"
-                    value={this.state.password}
-                    onChange={this.updateState}
-                  />
-                </label>
-                <button type="submit">Sign Up!</button>
-              </fieldset>
+                {/* Using IIFEs for transitioning to the next Sign-up Step and back to previous step*/}
+                {(() => {
+                  switch (step) {
+                    case 1:
+                      return (
+                        <Fragment>
+                          <FormGroup>
+                            <FormLabel htmlFor="name" width={'10rem'}>
+                              Name
+                            </FormLabel>
+                            <FormBox
+                              type="text"
+                              name="name"
+                              id="name"
+                              placeholder="Enter Name"
+                              value={this.state.name}
+                              onChange={this.updateState}
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <FormLabel htmlFor="email" width={'10rem'}>
+                              Email
+                            </FormLabel>
+                            <FormBox
+                              type="email"
+                              name="email"
+                              id="email"
+                              placeholder="Enter Email"
+                              value={this.state.email}
+                              onChange={this.updateState}
+                            />
+                          </FormGroup>
+                        </Fragment>
+                      );
+                    case 2:
+                      return (
+                        <Fragment>
+                          <FormGroup>
+                            <FormLabel htmlFor="password" width={'10rem'}>
+                              Password
+                            </FormLabel>
+                            <FormBox
+                              type="password"
+                              name="password"
+                              id="password"
+                              placeholder="Enter Password"
+                              value={this.state.password}
+                              onChange={this.updateState}
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <FormLabel htmlFor="password2" width={'15rem'}>
+                              Password Again
+                            </FormLabel>
+                            <FormBox
+                              type="password"
+                              name="password2"
+                              id="password2"
+                              placeholder="Re-Enter Password"
+                              value={this.state.password2}
+                              onChange={this.updateState}
+                            />
+                          </FormGroup>
+                        </Fragment>
+                      );
+                  }
+                })()}
+
+                {(() => {
+                  switch (step) {
+                    case 1:
+                      return <NextBtn onClick={this.nextStep}>Next</NextBtn>;
+                    case 2:
+                      return (
+                        <ButtonGroup>
+                          <BackBtn onClick={this.prevStep}>Back</BackBtn>
+                          <SignUpBtn type="submit">Sign Up!</SignUpBtn>
+                        </ButtonGroup>
+                      );
+                  }
+                })()}
+              </FormFieldset>
             </Form>
           );
         }}
