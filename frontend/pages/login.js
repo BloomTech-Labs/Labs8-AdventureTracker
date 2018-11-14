19f1e618c23630ef39af5fae1982a47825f40050
Simon Davis <Simon_Freelance@hotmail.com>
2018-11-13 19:27:33 -0500

    Removed Un-needed elements in login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,20 +2,9 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import Signin from '../components/Signin';
 
-const Columns = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 20px;
-`;
-
 const Login = () => (
   <div>
-    <Columns>
-      <Signin />
-    </Columns>
-    <Link href="/triplist">
-      <button>Complete</button>
-    </Link>
+    <Signin />
   </div>
 );
 
