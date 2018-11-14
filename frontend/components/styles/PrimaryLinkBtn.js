b77f977ce0f69fb9017b08a9ca207a1891667cf8
Simon Davis <Simon_Freelance@hotmail.com>
2018-11-13 19:25:28 -0500

    Added a way to customize PrimaryLinkBtn size

--- frontend/components/styles/PrimaryLinkBtn.js
+++ frontend/components/styles/PrimaryLinkBtn.js
@@ -3,8 +3,8 @@ const PrimaryLinkBtn = styled.a`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 6rem;
-  width: 18rem;
+  height: ${props => (props.height ? props.height : '6rem')};
+  width: ${props => (props.width ? props.width : '18rem')};
   border: none;
   border-radius: 10px;
   color: ${props => props.theme.white};
