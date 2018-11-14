36d88b91caecfb9a92ba19f2c890c42e06bf7843
Simon Davis <Simon_Freelance@hotmail.com>
2018-11-13 19:25:09 -0500

    PrimaryBtn added to ButtonStyles

---
+++ frontend/components/styles/ButtonStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const PrimaryBtn = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: ${props => (props.height ? props.height : '6rem')};
+  width: ${props => (props.width ? props.width : '18rem')};
+  border: none;
+  border-radius: 10px;
+  color: ${props => props.theme.white};
+  background: ${props => props.theme.orange};
+  cursor: pointer;
+  font-size: 3rem;
+`;
+
+export { PrimaryBtn };
