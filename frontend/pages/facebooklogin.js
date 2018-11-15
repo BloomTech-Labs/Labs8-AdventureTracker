fc579465ec74400233768aa5fe55d0d1d3fab93a
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-11-15 13:58:11 -0800

    start for implementing passport's Facebook strategy
    Co-authored-by: Simon <Simon_Freelance@hotmail.com>

---
+++ frontend/pages/facebooklogin.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import FacebookLogin from '../components/FacebookLogin';
+
+const FacebookLoginPage = () => (
+  <div>
+    <FacebookLogin />
+  </div>
+);
+
+export default FacebookLoginPage;
