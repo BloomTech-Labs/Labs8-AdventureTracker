aafc5cd89c9c2a5cf62cd4626f626bf524e12b9f
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-11-16 08:04:37 -0800

    facebook auth works! need to add local db parts
    Co-authored-by: Simon <Simon_Freelance@hotmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,23 @@
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook');
 const cookieParser = require('cookie-parser');
+var secure = require('express-force-https');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
+const express = server.express;
+
+// enforce HTTPS for all URLS
+express.use(secure);
 
 // Use express middleware to handle cookies (JTW)
-server.express.use(cookieParser());
+express.use(cookieParser());
 
 // decode JWT to get user ID with every req
-server.express.use((req, res, next) => {
+express.use((req, res, next) => {
   // get the token from req
   const { token } = req.cookies;
 
@@ -23,9 +30,48 @@ server.express.use((req, res, next) => {
 
     console.log('USERID', userId);
   }
-
   next();
 });
+express.use(passport.initialize());
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      // User grants permission and this is the callbackURL of what route
+      // we want the user to go through
+      callbackURL: 'https://adventuretracker.now.sh/auth/facebook/callback',
+      // callbackURL: '/auth/facebook/callback',
+      // Specifying what fields I want from Facebook
+      profileFields: ['id', 'displayName', 'email']
+    },
+    function(accessToken, refreshToken, profile, done) {
+      //This happens first before the callbackURL happens.
+      //The profile argument contains the needed information
+      // it is an object
+      done(null, profile);
+    }
+  )
+);
+// Once the user clicks the button, the user goes to facebook for authentication
+// passport handles the facebook authentication
+express.get('/auth/facebook', passport.authenticate('facebook'));
+
+express.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', { session: false }),
+  function(req, res) {
+    console.log('EVERYTHING GOOD TO GO!');
+    // req.user is the profile information
+    console.log(req.user);
+
+    // successful authentication, go to page we specify such as /triplist
+    // res.send('auth was good');
+    let postLoginPath = 'https://adventure-tracker-frontend.netlify.com/facebooklogin';
+    res.redirect(postLoginPath);
+  }
+);
 
 server.start(
   {
