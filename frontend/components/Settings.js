a9837c7807a58b25d98417f1c99a979edf8a6f8f
Simon Davis <simon_freelance@hotmail.com>
2018-11-26 13:07:24 -0500

    Added a way to inform user that passwords don't match.

--- frontend/components/Settings.js
+++ frontend/components/Settings.js
@@ -26,6 +26,9 @@ const PasswordTitle = styled(FormTitle)`
   line-height: 1;
   font-weight: 700;
 `;
+const PasswordBox = styled(FormBox)`
+  border: ${props => props.passwordMatch ? "none" : "3px solid red"};
+`;
 const SaveBtn = styled(PrimaryBtn)`
   margin: 0 0 0 auto;
   background: ${props => props.theme.orange};
@@ -44,12 +47,25 @@ class Settings extends Component {
   state = {
     oldPassword: '',
     newPassword: '',
-    email: ''
+    email: '',
+    passwordMatch: true,
   };
   saveToState = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ [e.target.name]: e.target.value }, () => this.passwordMatch());
   };
+  passwordMatch = () => {
+    const { oldPassword, newPassword } = this.state;
+    console.log(oldPassword === newPassword)
+    //Checks if they both have text and if they match or not
+    if(oldPassword && newPassword && oldPassword !== newPassword) {
+      this.setState({ passwordMatch: false });
+    }
+    else if(oldPassword === newPassword) {
+      this.setState({ passwordMatch: true });
+    }
+  }
   render() {
+    const { passwordMatch } = this.state;
     return (
       <Mutation
         mutation={CHANGE_PASSWORD_MUTATION}
@@ -75,26 +91,32 @@ class Settings extends Component {
                   <FormLabel htmlFor="oldPassword" width = {"13rem"}>
                     Old Password
                   </FormLabel>
-                  <FormBox
+                  <PasswordBox
                     type="password"
                     name="oldPassword"
                     placeholder="Enter Old Password"
                     id="oldPassword"
                     value={this.state.password}
-                    onChange={this.saveToState}
+                    passwordMatch={passwordMatch}
+                    onChange={(e) => {
+                      this.saveToState(e);
+                    }}
                   />
                 </FormGroup>
                 <FormGroup>
                   <FormLabel htmlFor="newPassword" width = {"13rem"}>
                     New Password
                   </FormLabel>
-                  <FormBox
+                  <PasswordBox
                     type="password"
                     name="newPassword"
                     placeholder="Enter New Password"
                     id="newPassword"
                     value={this.state.password}
-                    onChange={this.saveToState}
+                    passwordMatch={passwordMatch}
+                    onChange={(e) => {
+                      this.saveToState(e);
+                    }}
                   />
                 </FormGroup>
                 <SaveBtn type="submit">Save</SaveBtn>
