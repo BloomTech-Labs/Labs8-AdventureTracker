2b52269dc0a87fa98d72bdfb152ac852cd8f96b6
Lonnie Davis <ldavis0866@gmail.com>
2018-11-27 16:55:59 -0600

    deploy correction

--- frontend/components/Billing.js
+++ frontend/components/Billing.js
@@ -7,32 +7,28 @@ import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
 class Payment extends Component {
-
-    onToken = res => {
-        console.log('On Token Called!');
-        console.log(res.id);
-    };
-    render() {
-        return (
-        
-            <User>
-            {({ data: { me }}) => (
-            <StripeCheckout
+  onToken = res => {
+    console.log('On Token Called!');
+    console.log(res.id);
+  };
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
             stripeKey="pk_test_gdkcAGf0cgA2G1afawEyNJeg"
             currrency="USD"
             amount="999.99"
             // email={me.email}
             name="Adventure Tracker"
             token={res => this.onToken(res)}
-            >
+          >
             {this.props.children}
-            </StripeCheckout>
-            )}
-            </User>
-        );
-
-    }
-
+          </StripeCheckout>
+        )}
+      </User>
+    );
+  }
 }
 
 export default Payment;
