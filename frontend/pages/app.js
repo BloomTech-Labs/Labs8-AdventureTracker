fc2f092ac277fd17289d0d73a2498b47832d26e5
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2018-11-29 15:08:30 -0600

    app.js code for simon

--- frontend/pages/app.js
+++ frontend/pages/app.js
@@ -4,13 +4,85 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper, Polygon, Polyline } from 'go
 import CurrentLocation from './map';
 
 export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-    markers: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      markers: [],
+      polylines: []
+    };
+    this.NOT_STARTED = 'NOT_STARTED';
+    this.IN_PROGRESS = 'IN_PROGRESS';
+    this.COMPLETED = 'COMPLETED';
+  }
+  componentDidMount() {
+    // this.setState({ markers:  });
+  }
+  //distance in miles
+  //distance matrix
+  //create the ETA
+  addLines = markers => {
+    // thin grey is not reached yet and the person has not started that path
+    const greyLine = {
+      strokeWeight: 5,
+      strokeColor: '#969696'
+    };
+    // a dashed line is in-progress, the person has started on that path
+    // Doc for dashed-line: https://developers.google.com/maps/documentation/javascript/examples/overlay-symbol-dashed
+    const lineSymbol = {
+      path: 'M 0,-1 0,1',
+      strokeOpacity: 1,
+      scale: 4
+    };
+    const dashedLine = {
+      strokeOpacity: 0,
+      icons: [
+        {
+          icon: lineSymbol,
+          offset: '0',
+          repeat: '20px'
+        }
+      ]
+    };
+    // solid black line means the path was traversed
+    const solidBlackLine = {
+      strokeWeight: 7,
+      strokeColor: '#000000'
+    };
+    const lines = [];
+    let line = [];
+    for (let i = 0; i < markers.length; i++) {
+      let lineOptions = {};
+      //Depending on marker's status, this will choose what type of line to use
+      if (marker[i].status === this.NOT_STARTED) {
+        lineOptions = {
+          ...greyLine
+        };
+      } else if (marker[i].status === this.IN_PROGRESS) {
+        lineOptions = {
+          ...dashedLine
+        };
+      } else if (marker[i].status === this.COMPLETED) {
+        lineOptions = {
+          ...solidBlackLine
+        };
+      }
+      //set the lat and lng dot
+      line.push([markers[i].lat, markers[i].lng]);
 
+      //Every two markers set consecutively, add a new polyline
+      if (i > 0) {
+        lineOptions['path'] = line;
+        lines.push(lineOptions);
+        //Reset for new line
+        line = [[markers[i].lat, markers[i].lng]];
+      }
+    }
+
+    this.setState({ polylines: lines });
+  };
   mapClicked = (mapProps, map, clickEvent) => {
     console.log(clickEvent.xa);
     const myLatLng = {
@@ -24,14 +96,24 @@ export class MapContainer extends Component {
       position: myLatLng,
       map: map,
       id: amountOfMarkers,
-      title: amountOfMarkers,
-      label: labels[amountOfMarkers % labels.length]
+      title: String(amountOfMarkers),
+      label: labels[amountOfMarkers % labels.length],
+      //NOT_STARTED, IN_PROGRESS, COMPLETED - NOT_STARTED is default
+      status: this.NOT_STARTED
     });
-    markers.push(marker);
-    console.log(markers);
+    console.log(marker.position);
+    this.setState({ markers: [...markers, marker] });
+    // Line coords takes an array of arrays which specifies where the dots are
+    // Need to use the marker coordinates in order to make those lines
+    // Somehow specify what type of line it is
 
     const InfoWindow = new google.maps.InfoWindow({
-      content: myLatLng
+      content:
+        '<b>Marker Coordinates : </b> <br><b>Latitude : </b>' +
+        marker.position.lat() +
+        '<br><b>Longitude: </b>' +
+        marker.position.lng(),
+      position: marker.position
     });
     marker.addListener('click', function() {
       InfoWindow.open(map, marker);
@@ -39,7 +121,8 @@ export class MapContainer extends Component {
   };
 
   render() {
-    const { markers } = this.state;
+    const { markers, polylines } = this.state;
+
     return (
       <Map
         google={this.props.google}
@@ -53,11 +136,22 @@ export class MapContainer extends Component {
             <Marker
               title={mark.title}
               id={mark.id}
+              key={mark.key}
               position={{ lat: mark.position.lat, lng: mark.position.lng }}
             />
           );
         })}
-
+        {polylines.map((line, i) => {
+          return (
+            <Polyline
+              key={i}
+              path={line.path}
+              strokeColor={line.strokeColor}
+              strokeWeight={line.strokeWeight}
+            />
+          );
+        })}
+        }
         {/* <Marker
           title={'The marker`s title will appear as a tooltip.'}
           name={'SOMA'}
