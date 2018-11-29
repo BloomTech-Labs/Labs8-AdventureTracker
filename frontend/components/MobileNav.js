8034030e6ba2cbf26222c5122344ef3da5d2d739
Simon Davis <simon_freelance@hotmail.com>
2018-11-28 21:45:44 -0500

    Start of mobile menu completed

--- frontend/components/MobileNav.js
+++ frontend/components/MobileNav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { device } from '../lib/device';
+import { HamburgerIcon, ExitIcon } from './styles/SVGs';
 
 const MobileWrapper = styled.nav`
   display: none;
@@ -8,17 +9,39 @@ const MobileWrapper = styled.nav`
   bottom: 0;
   left: 0;
   right: 0;
-  height: 5rem;
+  height: 6rem;
   background: ${props => props.theme.orange};
   @media ${device.mobile} {
     display: flex;
+    justify-content: center;
   }
+`;
+const MenuBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: ${props => props.theme.orange};
+  border: none;
 `;
 
 class MobileNav extends Component {
-  state = {};
+  state = {
+    menuToggle: false
+  };
+  makeMenuActive = () => {
+    this.setState({ menuToggle: !this.state.menuToggle });
+  };
   render() {
-    return <MobileWrapper />;
+    const { menuToggle } = this.state;
+    console.log(menuToggle);
+    return (
+      <MobileWrapper>
+        <MenuBtn onClick={this.makeMenuActive}>
+          {menuToggle ? <ExitIcon /> : <HamburgerIcon />}
+        </MenuBtn>
+      </MobileWrapper>
+    );
   }
 }
 
