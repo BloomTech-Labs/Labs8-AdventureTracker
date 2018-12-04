cf347cff37fa9fc4c63670ef33643d3e032e2ba7
Simon Davis <simon_freelance@hotmail.com>
2018-12-04 18:54:09 -0500

    Added CommonTopNavbar to pages

--- frontend/pages/billing.js
+++ frontend/pages/billing.js
@@ -2,9 +2,11 @@ import Link from 'next/link';
 import Payment from '../components/Billing';
 import MobileNavContainer from '../components/MobileNavContainer';
 import SideNav from '../components/SideNav';
+import CommonTopNavbar from '../components/CommonTopNavbar';
 
 const Billing = () => (
   <div>
+    <CommonTopNavbar />
     <p>This is the Billing page</p>
     <Payment />
 
--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import SettingsComponent from '../components/Settings';
 import SideNav from '../components/SideNav';
+import CommonTopNavbar from '../components/CommonTopNavbar';
 const Settings = () => (
   <div>
+    <CommonTopNavbar />
     <p>This is the Settings page</p>
     <SideNav />
     <SettingsComponent />
