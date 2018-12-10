09a716dac2775ae1701fc85606e3db49d96b49c6
Simon Davis <simon_freelance@hotmail.com>
2018-12-10 15:19:56 -0500

    Specific trip query added

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,27 @@
 const { forwardTo } = require('prisma-binding');
 const Query = {
   trips: forwardTo('db'),
-  trip: forwardTo('db'),
+  // trip: forwardTo('db'),
   users: forwardTo('db'),
   async trips(parent, args, ctx, info) {
     const trips = await ctx.db.query.trips();
     return trips;
+  },
+  async trip(parent, args, ctx, info) {
+    // const user = ctx.db.query.user({
+    //   where: {id: ctx.request.userId}
+    // });
+    // if(!user) {
+    //   throw new Error('Please login to access the trip for that user.');
+    // }
+    console.log(args);
+    const trip = await ctx.db.query.trip({
+      where: {
+        id: args.where.id
+      }
+    });
+
+    return trip;
   },
   me(parent, args, ctx, info) {
     // check if there is a current user ID
