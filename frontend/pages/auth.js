a5882cb8ac68e605b3cabbc8fbdfc55034796293
Simon Davis <simonfreelance1@gmail.com>
2019-04-07 15:22:55 -0400

    Embedded video and made title change depending on tab

--- frontend/pages/auth.js
+++ frontend/pages/auth.js
@@ -4,6 +4,7 @@ import {Card, Button, Icon} from "antd";
 import {useState} from "react";
 import styled from "styled-components";
 import homeImg from "../static/jumbotron-home.jpg";
+import {Divider} from "antd";
 
 const AuthPage = () => {
   const LOGIN = "login";
@@ -11,32 +12,45 @@ const AuthPage = () => {
   const [tab, setTab] = useState(LOGIN);
   return (
     <AuthPageWrapper>
-      <AuthCard>
-        <SignupForm isVisible={tab === SIGNUP ? true : false} />
-        <LoginForm isVisible={tab === LOGIN ? true : false} />
+      <Twin>
+        <AuthCard title={tab === SIGNUP ? "Signup" : "Login"}>
+          <SignupForm isVisible={tab === SIGNUP ? true : false} />
+          <LoginForm isVisible={tab === LOGIN ? true : false} />
 
-        <AuthTabGroup size={"large"}>
-          <Tab
-            type={tab === SIGNUP ? "primary" : ""}
-            onClick={() => setTab(SIGNUP)}
-          >
-            <Icon type="user-add" />
-            Signup
-          </Tab>
-          <Tab
-            type={tab === LOGIN ? "primary" : ""}
-            onClick={() => setTab(LOGIN)}
-          >
-            <Icon type="login" />
-            Login
-          </Tab>
-        </AuthTabGroup>
-      </AuthCard>
+          <AuthTabGroup size={"large"}>
+            <Tab
+              type={tab === SIGNUP ? "primary" : ""}
+              onClick={() => setTab(SIGNUP)}
+            >
+              <Icon type="user-add" />
+              Signup
+            </Tab>
+            <Tab
+              type={tab === LOGIN ? "primary" : ""}
+              onClick={() => setTab(LOGIN)}
+            >
+              <Icon type="login" />
+              Login
+            </Tab>
+          </AuthTabGroup>
+        </AuthCard>
+        <Divider type="vertical" />
+        <IntroVideo
+          src="https://www.youtube.com/embed/pX7lvQyMvtA"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </Twin>
       <BackgroundImage src={homeImg} />
     </AuthPageWrapper>
   );
 };
 
+const IntroVideo = styled.iframe`
+  width: 100%;
+  min-height: 100%;
+`;
 const Title = styled.h1`
   position: absolute;
   left: 50%;
@@ -58,7 +72,16 @@ const BackgroundImage = styled.img`
   z-index: -1000;
   filter: brightness(50%);
 `;
-const Twin = styled.div``;
+const Twin = styled.div`
+  display: flex;
+  max-width: 1000px;
+  width: 100%;
+  margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 const AuthCard = styled(Card)`
   max-width: 500px;
   width: 100%;
