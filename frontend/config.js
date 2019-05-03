f3603e8c4eea83c4c599544e1b82feea8c8a3122
Simon Davis <simonfreelance1@gmail.com>
2019-05-02 22:39:23 -0400

    test prodEndpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,9 +1,8 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-export const endpoint = `https://adventure-tracker-7db8b3cc51.herokuapp.com/adventuretracker/dev`;
+export const endpoint = `https://node-adventuretracker.herokuapp.com/`;
 // export const endpoint = `http://localhost:4444`;
+
 export const prodEndpoint =
-  "https://us1.prisma.sh/thomas-greenhalgh/AdventureTracker/dev";
-export const prodEndpoint =
-"https://adventure-tracker-7db8b3cc51.herokuapp.com/adventuretracker/dev";
+"https://node-adventuretracker.herokuapp.com/";
 export const perPage = 4;
