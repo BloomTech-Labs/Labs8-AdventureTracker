d2622a35b7fb95b00353138fb483d64b52f67264
Simon Davis <simonfreelance1@gmail.com>
2019-05-03 12:35:00 -0400

    temporary local endpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-export const endpoint = `https://adventure-tracker-7db8b3cc51.herokuapp.com/adventuretracker/dev`;
+export const endpoint = `https://node-adventuretracker.herokuapp.com/`;
 // export const endpoint = `http://localhost:4444`;
 
 export const prodEndpoint = "https://node-adventuretracker.herokuapp.com/";
