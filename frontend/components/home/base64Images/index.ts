87f29c01c484d118ca1b96e0402f339de4bd79f3
Simon Davis <simonfreelance1@gmail.com>
2019-05-20 13:05:12 -0400

    Added starting base64 image data

---
+++ frontend/components/home/base64Images/index.ts
@@ -0,0 +1,6 @@
+export {
+  default as blurAdventureTrackerJumbo,
+} from "./blurAdventureTrackerJumbo";
+export {
+  default as blurNorthAmericaWithMarkers,
+} from "./blurNorthAmericaWithMarkers";
