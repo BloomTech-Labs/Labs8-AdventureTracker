95660a0781834bb0092d475b3ca455f241207099
Simon Davis <simonfreelance1@gmail.com>
2019-05-22 12:21:56 -0400

    ReachedCheckBox works again and has been refactored

--- frontend/components/map/InfoWindow/ReachedCheckbox.tsx
+++ frontend/components/map/InfoWindow/ReachedCheckbox.tsx
@@ -1,66 +1,71 @@
 import {useContext} from "react";
-import {showConfirmModal} from "../antModals";
-import {changeMarkersProps} from "../helper-functions/index";
-import {Checkbox} from "antd";
+import {Checkbox, Modal} from "antd";
 import {Marker} from "../interfaces/marker.interface";
 import MapContext from "../../context/MapContext";
 import {GREY_PIN, CHECKED_PIN} from "../map-icons/markerIcons";
-type ContextTypes = {
-  markers: [Marker];
-  setMarkers: any;
-  setActiveMarker: any;
-  toggleMarkerReached: any;
-  activeMarker: Marker;
-};
+const confirm = Modal.confirm;
 const ReachedCheckbox = () => {
-  // const {
-  //   markers,
-  //   setMarkers,
-  //   setActiveMarker,
-  //   toggleMarkerReached,
-  //   activeMarker,
-  // }: ContextTypes = useContext(MapContext);
   const {markState, markDispatch} = useContext(MapContext);
-  const {activeMarker} = markState;
+  const {activeMarker, markers} = markState;
   return (
     <Checkbox
-      onChange={() => {
-        // const prevReachedConfirm = showConfirmModal.bind(
-        //   null,
-        //   "Are you sure you reached this point?",
-        //   "Previous markers will be confirmed as reached.",
-        //   (startIndex: number, endIndex: number) => {
-        //     const newMarkers = changeMarkersProps(
-        //       markers,
-        //       {hasReached: true, url: CHECKED_PIN},
-        //       startIndex,
-        //       endIndex,
-        //     );
-        //     setMarkers(newMarkers);
-        //     setActiveMarker(newMarkers[endIndex]);
-        //   },
-        // );
-        // const nextReachedConfirm = showConfirmModal.bind(
-        //   null,
-        //   "Are you sure you want to backtrack?",
-        //   "Markers ahead of this location will be marked as not reached.",
-        //   (startIndex: number, endIndex: number) => {
-        //     const newMarkers = changeMarkersProps(
-        //       markers,
-        //       {hasReached: false, url: GREY_PIN},
-        //       startIndex,
-        //       endIndex,
-        //     );
-        //     setMarkers(newMarkers);
-        //     setActiveMarker(newMarkers[startIndex]);
-        //   },
-        // );
-        // toggleMarkerReached(
-        //   activeMarker.id,
-        //   prevReachedConfirm,
-        //   nextReachedConfirm,
-        // );
-        // console.log(e.target.checked);
+      onChange={(e: any) => {
+        const activeIndex = markers.findIndex((mark: Marker) => {
+          return mark.id === activeMarker.id;
+        });
+
+        if (
+          markers[activeIndex - 1] &&
+          markers[activeIndex - 1].hasReached === false &&
+          activeMarker.hasReached === false
+        ) {
+          confirm({
+            title: "Are you sure you reached this point?",
+            content: "Previous markers will be confirmed as reached.",
+            onOk() {
+              for (let i = 0; i <= activeIndex; i++) {
+                markDispatch({
+                  type: "UPDATE_MARKER",
+                  marker: markers[i],
+                  props: {
+                    hasReached: true,
+                    url: CHECKED_PIN,
+                  },
+                });
+              }
+            },
+          });
+        } else if (
+          activeMarker.hasReached &&
+          markers[activeIndex + 1].hasReached
+        ) {
+          confirm({
+            title: "Are you sure you want to backtrack to this marker?",
+            content:
+              "Markers ahead of the current marker will be confirmed as not reached.",
+            onOk() {
+              for (let i = markers.length - 1; i >= activeIndex; i--) {
+                markDispatch({
+                  type: "UPDATE_MARKER",
+                  marker: markers[i],
+                  props: {
+                    hasReached: false,
+                    url: GREY_PIN,
+                  },
+                });
+              }
+            },
+          });
+        } else {
+          markDispatch({
+            type: "UPDATE_MARKER",
+            marker: activeMarker,
+            props: {
+              hasReached: e.target.checked,
+              url: e.target.checked ? CHECKED_PIN : GREY_PIN,
+            },
+          });
+        }
       }}
       checked={activeMarker.hasReached}
     >
