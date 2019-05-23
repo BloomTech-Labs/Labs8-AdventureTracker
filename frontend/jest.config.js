cee420b56a73d81234190142a98bdabe7af980a5
Simon Davis <simonfreelance1@gmail.com>
2019-05-23 18:31:12 -0400

    Got basic jest configured with typescript

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+const {defaults} = require("jest-config");
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+};
