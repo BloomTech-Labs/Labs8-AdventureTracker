3804ac0665577d72a5a49ddabc7d421fba3d6e12
Simon Davis <simonfreelance1@gmail.com>
2019-05-25 23:06:19 -0400

    Attempt 1 to fix id query paraemeter to production

--- frontend/pages/map.js
+++ frontend/pages/map.js
@@ -4,9 +4,26 @@ import {withRouter} from "next/router";
 
 const Map = props => {
   const {query} = props.router;
+  console.log({router: props.router});
+  if (!query.id && process.env.NODE_ENV === "production") {
+    const productionQueryId =
+      props.router.asPath.match(/\/map\/\?id=(\w+)/)[1] || "";
+    console.log("productionQueryId", productionQueryId);
+  }
   return (
     <ApolloConsumer>
-      {client => <MapComponent client={client} tripId={query.id} />}
+      {client => (
+        <MapComponent
+          client={client}
+          tripId={
+            query.id
+              ? query.id
+              : process.env.NODE_ENV === "production"
+              ? productionQueryId
+              : ""
+          }
+        />
+      )}
     </ApolloConsumer>
   );
 };
