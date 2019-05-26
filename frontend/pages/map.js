e1a7da2e715efe483b62e6d44820de66f045535e
Simon Davis <simonfreelance1@gmail.com>
2019-05-25 23:23:18 -0400

    Fixed scope

--- frontend/pages/map.js
+++ frontend/pages/map.js
@@ -5,12 +5,13 @@ import {withRouter} from "next/router";
 const Map = props => {
   const {query, asPath} = props.router;
   // console.log({router: props.router});
+  let productionQueryId = "";
   if (
     !query.id &&
     asPath.match(/\/map\/\?id=(\w+)/) &&
     process.env.NODE_ENV === "production"
   ) {
-    const productionQueryId = asPath.match(/\/map\/\?id=(\w+)/)[1] || "";
+    const productionQueryId = asPath.match(/\/map\/\?id=(\w+)/)[1];
     console.log("productionQueryId", productionQueryId);
   }
   return (
