1d050682a6fb75d038acd66f31761f4c3320598b
Simon Davis <simonfreelance1@gmail.com>
2019-05-26 19:14:22 -0400

    Display no label when no text in label

--- frontend/components/map/MapComponent.tsx
+++ frontend/components/map/MapComponent.tsx
@@ -221,7 +221,7 @@ const MapComponent = compose(
                 26,
               )
             }
-            labelStyle={mark.labelStyle}
+            labelStyle={mark.label.length ? mark.labelStyle : ""}
             icon={{
               origin: new google.maps.Point(0, 0),
               url: mark.url,
